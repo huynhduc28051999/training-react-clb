797baf4aaddeadd55ea9ea06d54aee965b6d68b0
huynhduc280519999 <huynhduc28051999@gmail.com>
2021-03-17 20:32:19 +0700

    axios

--- src/App.js
+++ src/App.js
@@ -1,59 +1,79 @@
 import React, { Component } from 'react'
 import './app.scss'
-import avartar from './avatar.png'
+import axiosClient from './axiosClient'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isAuth: localStorage.getItem('token'),
+      listGroup: []
+    }
+  }
+  componentDidMount() {
+    this.getAllGroup()
+  }
+  getAllGroup = () => {
+    axiosClient.get('/group/get-all-group')
+    .then((data) => {
+      this.setState({ listGroup: data.data })
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+  handleSubmit = (e) => {
+    if (e) e.preventDefault()
+    const formValue = new FormData(e.target)
+    const value = {}
+    for (var key of formValue.entries()) {
+      value[key[0]] = key[1]
+    }
+    axiosClient.post('/auth/login', value)
+    .then((data) => {
+      console.log(data.token);
+      localStorage.setItem('token', data.token)
+      this.setState({ isAuth: true })
+    })
+    .catch(function (error) {
+      this.setState({ isAuth: false })
+    });
+  }
   render() {
     return (
-      <div className='intro-myself'>
-        <div className="container">
-          <div className='intro-myself__basic'>
-            <div className='intro-myself__basic__avatar'>
-              <img src={avartar} alt='avatar' />
-            </div>
-            <div className='intro-myself__basic__content'>
-              <div className='intro-myself__basic__content__title'>Hello world</div>
-              <div className='intro-myself__basic__content__subTittle'>My name is ...</div>
-            </div>
+      <div>
+        {!this.state.isAuth ? (
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor='username'>email</label>
+            <input id='username' name='email' /><br/>
+            <label htmlFor='password'>password</label>
+            <input id='password' name='password' /><br/>
+            <input type='submit' name='submit'/>
+          </form>
+        ) : (
+          <div style={{ width: '100%' }}>
+            <table style={{ width: '100%' }}>
+              <thead>
+                <tr>
+                  <th>Tên</th>
+                  <th>avatar</th>
+                  <th>Mô tả</th>
+                  <th>action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.listGroup.map((item, idx) => (
+                  <tr key={idx}>
+                    <td>{item.name}</td>
+                    <td>{item.avatar}</td>
+                    <td>{item.description}</td>
+                    <td><button>xóa</button></td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
-          <div className='intro-myself__content'>
-            <div className='intro-myself__content__tile'>
-              <h2>Introducing Yourself Self</h2>
-              <h2>Introducing with Text Boxes</h2>
-            </div>
-            <div className='intro-myself__content__line'>
-              <div className='circle left' />
-              <div className='line' />
-              <div className='circle right' />
-            </div>
-            <div className='intro-myself__content__container'>
-              <div className='boxes'>
-                <div className='boxes-container'>
-                  <h3>TextHere 1</h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam cum magnam minus neque totam sint quo excepturi.</p>
-                </div>
-              </div>
-              <div className='boxes'>
-                <div className='boxes-container'>
-                  <h3>TextHere 2</h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam cum magnam minus neque totam sint quo excepturi.</p>
-                </div>
-              </div>
-              <div className='boxes'>
-                <div className='boxes-container'>
-                  <h3>TextHere 3</h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam cum magnam minus neque totam sint quo excepturi.</p>
-                </div>
-              </div>
-              <div className='boxes'>
-                <div className='boxes-container'>
-                  <h3>TextHere 4</h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam cum magnam minus neque totam sint quo excepturi.</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        )}
       </div>
     )
   }
